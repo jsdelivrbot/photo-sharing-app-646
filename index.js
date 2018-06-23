cb35379a52cdac020440dca0f809c4acb9c05eed
eric nichols <ericnichols@erics-MacBook-Pro.local>
2018-06-22 21:09:19 -0400

    latest

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require('express'),
 path = require('path'),
 PORT = process.env.PORT || 5000,
 config = require('./config')
-knox = require('knox')
+knox = require('knox'),
+fs = require('fs'),
+os = require('os'),
+formidable = require('formidable'),
+gm = require('gm'),
+mongoose = require('mongoose').connect("mongodb://ebn646:enen6767@ds159110.mlab.com:59110/photogrid646")
 
 var app = express();
 
@@ -21,12 +26,12 @@ var knoxClient = knox.createClient({
 	bucket: "photogrid646"
 })
 
-  //declare router and render index page
-
-require('./routes')(express, app);
+//declare router and render index page
 
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
+require('./routes')(express, app, formidable, fs, os, gm, knoxClient, mongoose, io);
 
 server.listen(app.get('port'), () => {
   console.log(`Photogrid Listening on ${ PORT }`)
