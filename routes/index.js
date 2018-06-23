a90de6974669fc3692c91246d90ebd71f89d75a6
eric nichols <ericnichols@erics-MacBook-Pro.local>
2018-06-22 22:19:02 -0400

    latest

--- routes/index.js
+++ routes/index.js
@@ -18,8 +18,33 @@ module.exports = (express, app, formidable, fs, os, gm, knoxClient, mongoose, io
 
     var router = express.Router();
 
+    var host = app.get('host')
+
     router.get('/', function (req, res, next) {
-        res.render('index', { host: app.get('host') });
+        let blogPosts = [
+            {
+                title: 'Perk is for real!',
+                body: '...',
+                author: 'Aaron Larner',
+                publishedAt: new Date('2016-03-19'),
+                createdAt: new Date('2016-03-19')
+            },
+            {
+                title: 'Development continues...',
+                body: '...',
+                author: 'Aaron Larner',
+                publishedAt: new Date('2016-03-18'),
+                createdAt: new Date('2016-03-18')
+            },
+            {
+                title: 'Welcome to Perk!',
+                body: '...',
+                author: 'Aaron Larner',
+                publishedAt: new Date('2016-03-17'),
+                createdAt: new Date('2016-03-17')
+            }
+        ]
+        res.render('index', { posts: blogPosts });
     });
 
     router.post('/upload', function (req, res, next) {
